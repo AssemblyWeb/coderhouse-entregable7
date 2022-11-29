d2763f33bf13cd34680413321ded6d34f05c1089
Alfio Perino <alfio@assemblyweb.net>
2022-11-29 15:21:19 -0300

    set sqlite for chat messages

--- model/chat/knexfile.js
+++ model/chat/knexfile.js
@@ -2,6 +2,6 @@ const path = require("path");
 
 module.exports = {
     client: "sqlite3",
-    connection: { filename: path.resolve(__dirname, './model/chat/chatdb.sqlite') },
+    connection: { filename: path.resolve(__dirname + '/coder_chat.sqlite') },
     useNullAsDefault: true
 }
